5257d319e23eab859945ff714404ff2b36096e1f
reiya023 <xqmp71180@mineo.jp>
2024-08-28 16:55:42 +0900

    ファイル名の変更とそれに伴う変更

--- scenes/game/card/pa.rb
+++ scenes/game/card/pa.rb
@@ -1,8 +1,8 @@
 module Scenes
   module Game
     module Card
-      class Diamond < Base
-        SUIT_MARK = "♦"
+      class Pa < Base
+        SUIT_MARK = "♥"
         SUIT_COLOR = 0xff_ff0000
         NUMBER_COLOR = 0xff_ff0000
       end
